2576a691f32eff2d15205173be72023f36f17bd6
15527988939 <mushan_wangyi@163.com>
2022-10-18 15:45:39 +0800

    10.18 homework7 document improve castRay() function to get better pictures

--- 宋智彬_Homework7/Renderer.cpp
+++ 宋智彬_Homework7/Renderer.cpp
@@ -28,7 +28,7 @@ void Renderer::Render(const Scene& scene)
     int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 64;   // origin is 16
+    int spp = 16;   // origin is 16
     std::cout << "SPP: " << spp << "\n";
 
 /*
