b9901dd96529417fc530ee18aa2caa8454918e16
15527988939 <mushan_wangyi@163.com>
2022-11-11 08:59:30 +0800

    11.11 upload homework8 coding task

--- Homework8/Assignment8/src/application.cpp
+++ Homework8/Assignment8/src/application.cpp
@@ -23,9 +23,9 @@ void Application::init() {
 
   glColor3f(1.0, 1.0, 1.0);
   // Create two ropes 
-  ropeEuler = new Rope(Vector2D(0, 200), Vector2D(-400, 200), 3, config.mass,
+  ropeEuler = new Rope(Vector2D(0, 200), Vector2D(-400, 200), 16, config.mass,       // origin num_nodes = 3; changed to 16
                        config.ks, {0});
-  ropeVerlet = new Rope(Vector2D(0, 200), Vector2D(-400, 200), 3, config.mass,
+  ropeVerlet = new Rope(Vector2D(0, 200), Vector2D(-400, 200), 16, config.mass,      // origin num_nodes = 3; changed to 16
                         config.ks, {0});
 }
 
@@ -67,8 +67,9 @@ void Application::render() {
 
     glEnd();
 
-    glFlush();
+    //glFlush();
   }
+  glFlush();
 }
 
 void Application::resize(size_t w, size_t h) {
--- Homework8/Assignment8/src/rope.cpp
+++ Homework8/Assignment8/src/rope.cpp
@@ -17,6 +17,18 @@ namespace CGL {
 //        for (auto &i : pinned_nodes) {
 //            masses[i]->pinned = true;
 //        }
+        for (int i = 0; i < num_nodes; ++i)
+        {
+            masses.push_back(new Mass(start + (end-start) / (num_nodes-1) * i, node_mass, false));
+        }
+        for (auto& i : pinned_nodes)
+        {
+            masses[i]->pinned = true;
+        }
+        for (int i = 0; i < num_nodes-1; ++i)
+        {
+            springs.push_back(new Spring(masses[i], masses[i+1], k));
+        }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
@@ -24,7 +36,12 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            auto len = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / len * (len - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / len * (len - s->rest_length);
         }
+
+        const float kd = 0.005;
 
         for (auto &m : masses)
         {
@@ -32,7 +49,18 @@ namespace CGL {
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
 
+                /*  // For explicit Euler method
+                auto a = m->forces / m->mass + gravity;
+                m->position += m->velocity * delta_t;
+                m->velocity += a * delta_t; */
+
+                // For semi-implicit Euler method
+                auto a = m->forces / m->mass + gravity - kd * m->velocity / m->mass;
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
+
                 // TODO (Part 2): Add global damping
+
             }
 
             // Reset all forces on each mass
@@ -45,7 +73,12 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            auto len = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / len * (len - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / len * (len - s->rest_length);
         }
+
+        const float damping_factor = 0.00005;
 
         for (auto &m : masses)
         {
@@ -53,9 +86,13 @@ namespace CGL {
             {
                 Vector2D temp_position = m->position;
                 // TODO (Part 3.1): Set the new position of the rope mass
-                
+                auto a = m->forces / m->mass + gravity;
+                m->position = temp_position + (1 - damping_factor) * (temp_position - m->last_position) + a * delta_t * delta_t;
+                m->last_position = temp_position;
                 // TODO (Part 4): Add global Verlet damping
+
             }
+            m->forces = Vector2D(0, 0);
         }
     }
 }
